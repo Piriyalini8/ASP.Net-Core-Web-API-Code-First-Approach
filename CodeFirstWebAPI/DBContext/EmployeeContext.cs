e6dfef40d991c101ff2e39594c4073e395a2a974
Piriyalini Niththiyanantham <priyalini@unicomtechnology.co.uk>
2022-06-17 06:46:36 +0530

    Add project files.

---
+++ CodeFirstWebAPI/DBContext/EmployeeContext.cs
@@ -0,0 +1,36 @@
+﻿using CodeFirstWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CodeFirstWebAPI.DBContext
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "Priyalini",
+                LastName = "Niththiyanantham",
+                Email = "priyalini8@gmail.com",
+                DateOfBirth = new DateTime(1997, 05, 26),
+                PhoneNumber = "076-888-7777"
+            }, new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Kishan",
+                LastName = "Vadivel",
+                Email = "kishanv@gmail.com",
+                DateOfBirth = new DateTime(1997, 05, 09),
+                PhoneNumber = "076-222-3333"
+            });
+        }
+    }
+}
---
+++ CodeFirstWebAPI/Migrations/20220617005930_SeedEmployeeData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeFirstWebAPI.Migrations
+{
+    public partial class SeedEmployeeData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "DateOfBirth", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[] { 1L, new DateTime(1997, 5, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), "priyalini8@gmail.com", "Priyalini", "Niththiyanantham", "076-888-7777" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "DateOfBirth", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[] { 2L, new DateTime(1997, 5, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), "kishanv@gmail.com", "Kishan", "Vadivel", "076-222-3333" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 2L);
+        }
+    }
+}
